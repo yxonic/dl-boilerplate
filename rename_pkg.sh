857bc78ec84da13a7d6a2d53e6fb28cae15799a2
Yu Yin <yxonic@gmail.com>
2018-12-04 14:33:36 +0800

    fix rename script

--- rename_pkg.sh
+++ rename_pkg.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 origname='app'
+
 if [ -z $1 ]; then
     echo 'usage: ./rename_pkg.sh <new_name>'
     exit 1
 fi
+
+if [ $1 == $origname ]; then
+    echo 'nothing to do'
+    exit
+fi
 newname=$1
 
-find docs/source $origname tests \( ! -name *.pyc \) -type f -exec perl -i -pe "s/$origname/$newname/g" {} +
-perl -i -pe "s/$origname/$newname/g" docs/Makefile * .* 2>/dev/null
+git ls-files -X .gitignore -co | grep -v \.md | tr '\n' '\0' | xargs -0 perl -i -pe "s/$origname/$newname/g" 2>/dev/null
+
 mv $origname $newname
 mv docs/source/$origname.rst docs/source/$newname.rst
